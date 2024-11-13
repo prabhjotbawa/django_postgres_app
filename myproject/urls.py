090c220501b59eae3bda8a78c1a6e5af1765bc6d
Prabhjot Singh Bawa <prabhjotbawa@gmail.com>
2024-11-13 18:20:08 -0400

    Fix template

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,15 +1,16 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from myapp.views import home, get_data_inserted, debug_view, get_metrics, health_check
+from myproject import settings
 
 urlpatterns = ([
-    path('api/', include('myapp.urls')),
-    path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    path('', include('django_prometheus.urls')),
-    path('data/', get_data_inserted),
-    path('debug/', debug_view),
-    path('custom-metrics/', get_metrics),
-    path('health/', health_check, name='health_check'),
-])
-# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
+                   path('api/', include('myapp.urls')),
+                   path('admin/', admin.site.urls),
+                   path('', home, name='home'),
+                   path('', include('django_prometheus.urls')),
+                   path('data/', get_data_inserted),
+                   path('debug/', debug_view),
+                   path('custom-metrics/', get_metrics),
+                   path('health/', health_check, name='health_check'),
+               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
